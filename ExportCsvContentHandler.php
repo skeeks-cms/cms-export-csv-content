cdab6f55dc2a901792aa34975ad33d64e6efb2fd
Semenov <semenov@skeeks.com>
2016-09-06 14:30:50 +0300

    1

--- ExportCsvContentHandler.php
+++ ExportCsvContentHandler.php
@@ -8,6 +8,7 @@
 namespace skeeks\cms\exportCsvContent;
 
 use skeeks\cms\export\ExportHandler;
+use skeeks\cms\export\ExportHandlerFilePath;
 use skeeks\cms\importCsv\handlers\CsvHandler;
 use skeeks\cms\importCsv\helpers\CsvImportRowResult;
 use skeeks\cms\importCsv\ImportCsvHandler;
@@ -20,6 +21,7 @@ use skeeks\cms\relatedProperties\propertyTypes\PropertyTypeElement;
 use skeeks\cms\relatedProperties\propertyTypes\PropertyTypeList;
 use yii\base\Exception;
 use yii\helpers\ArrayHelper;
+use yii\helpers\FileHelper;
 use yii\helpers\Json;
 use yii\widgets\ActiveForm;
 
@@ -33,6 +35,21 @@ use yii\widgets\ActiveForm;
 class ExportCsvContentHandler extends ExportHandler
 {
     public $content_id = null;
+
+    public $file_path = '';
+
+    public function init()
+    {
+        $this->name = \Yii::t('skeeks/exportCsvContent', '[CSV] Export content items');
+
+        if (!$this->file_path)
+        {
+            $rand = \Yii::$app->formatter->asDate(time(), "Y-M-d") . "-" . \Yii::$app->security->generateRandomString(5);
+            $this->file_path = "/export/content/content-{$rand}.csv";
+        }
+
+        parent::init();
+    }
 
     public function getAvailableFields()
     {
@@ -68,12 +85,6 @@ class ExportCsvContentHandler extends ExportHandler
         return CmsContent::findOne($this->content_id);
     }
 
-    public function init()
-    {
-        parent::init();
-
-        $this->name = \Yii::t('skeeks/exportCsvContent', '[CSV] Export content items');
-    }
 
 
     /**
@@ -119,7 +130,77 @@ class ExportCsvContentHandler extends ExportHandler
             'size' => 1,
             'data-form-reload' => 'true'
         ]);
+    }
 
 
+
+    public function export()
+    {
+        ini_set("memory_limit","8192M");
+        set_time_limit(0);
+
+        //Создание дирректории
+        if ($dirName = dirname($this->rootFilePath))
+        {
+            $this->result->stdout("Создание дирректории\n");
+
+            if (!is_dir($dirName) && !FileHelper::createDirectory($dirName))
+            {
+                throw new Exception("Не удалось создать директорию для файла");
+            }
+        }
+
+
+
+        $elements = CmsContentElement::find()->where([
+            'content_id' => $this->content_id
+        ])->all();
+
+        $countTotal = count($elements);
+        $this->result->stdout("\tЭлементов найдено: {$countTotal}\n");
+
+
+
+        $element = $elements[0];
+
+        $fp = fopen($this->rootFilePath, 'w');
+
+        $head = [];
+        foreach ($element->toArray() as $code => $value)
+        {
+            $head[] = "element." . $code;
+        }
+        /**
+         * @var $element CmsContentElement
+         */
+        foreach ($element->relatedPropertiesModel->toArray() as $code => $value)
+        {
+            $head[] = 'property.' . $code;
+        }
+
+        fputcsv($fp, $head, ";");
+
+        foreach ($elements as $element)
+        {
+            $propertiesRow = [];
+            foreach ($element->relatedPropertiesModel->toArray() as $code => $value)
+            {
+                $value = $element->relatedPropertiesModel->getSmartAttribute($code);
+                if (is_array($value))
+                {
+                    $value = implode(', ', $value);
+                }
+
+                $propertiesRow[$code] = $value;
+            }
+
+            $row = array_merge($element->toArray(), $propertiesRow);
+
+            fputcsv($fp, $row, ";");
+        }
+
+        fclose($fp);
+
+        return $this->result;
     }
 }
