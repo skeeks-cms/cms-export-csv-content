8570fe424249e66106c8c5ddcd66af4cb8f25fc6
Semenov <semenov@skeeks.com>
2016-09-07 18:15:57 +0300

    dev

--- config/main-console.php
+++ config/main-console.php
@@ -1,2 +1,29 @@
 <?php
-return [];
+return [
+
+    'components' =>
+    [
+        'cmsExport' => [
+            'handlers'     =>
+            [
+                'skeeks\cms\exportCsvContent\ExportCsvContentHandler' =>
+                [
+                    'class' => 'skeeks\cms\exportCsvContent\ExportCsvContentHandler'
+                ]
+            ]
+        ],
+
+        'i18n' => [
+            'translations' =>
+            [
+                'skeeks/exportCsvContent' => [
+                    'class'             => 'yii\i18n\PhpMessageSource',
+                    'basePath'          => '@skeeks/cms/exportCsvContent/messages',
+                    'fileMap' => [
+                        'skeeks/exportCsvContent' => 'main.php',
+                    ],
+                ]
+            ]
+        ]
+    ]
+];
